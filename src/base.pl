888c38e4a3f050a3a1cb076ccdd5e70b68af8078
LFMP <luizfellipe42@hotmail.com>
2020-01-01 22:33:43 -0300

    reorganize and finalize core functions

--- src/base.pl
+++ src/base.pl
@@ -4,53 +4,45 @@ doenca(deprecao).
 doenca(ansiedade).
 doenca(stress).
 
-% Atribuicao generica de nivel da doenca
-
-level('','') :- !.
-
-level(Y,Z) :-
-	Y = Z,
-	!.
-
 % O grau de cada doenca teve como base o questionario DASS 21
 % a soma maxima para cada doenca eh de 21 pontos
 
-grau(doenca(deprecao),X,Y) :-
+grau(doenca(deprecao),X) :-
 	X < 7,
-	level(Y,'normal'), !.
+	write_ln('Não há transtorno').
 
-grau(doenca(deprecao),X,Y) :-
+grau(doenca(deprecao),X) :-
 	X >= 7,
 	X < 14,
-	level(Y,'moderate'), !.
+	write_ln('Nível médio de transtorno').
 
-grau(doenca(deprecao),X,Y) :-
+grau(doenca(deprecao),X) :-
 	X >= 14,
-	level(Y,'severe'), !.
+	write_ln('Nível alto de transtorno').
 
-grau(doenca(ansiedade),X,Y) :-
+grau(doenca(ansiedade),X) :-
 	X < 6,
-	level(Y,'normal'), !.
+	write_ln('Não há transtorno').
 
-grau(doenca(ansiedade),X,Y) :-
+grau(doenca(ansiedade),X) :-
 	X >= 6,
 	X < 10,
-	level(Y,'moderate'), !.
+	write_ln('Nível médio de transtorno').
 
-grau(doenca(ansiedade),X,Y) :-
+grau(doenca(ansiedade),X) :-
 	X >= 10,
-	level(Y,'severe'), !.
+	write_ln('Nível alto de transtorno').
 
 
-grau(doenca(ansiedade),X,Y) :-
+grau(doenca(stress),X) :-
 	X < 10,
-	level(Y,'normal'), !.
+	write_ln('Não há transtorno').
 
-grau(doenca(ansiedade),X,Y) :-
+grau(doenca(stress),X) :-
 	X >= 10,
 	X < 17,
-	level(Y,'moderate'), !.
+	write_ln('Nível médio de transtorno').
 
-grau(doenca(ansiedade),X,Y) :-
+grau(doenca(stress),X) :-
 	X >= 17,
-	level(Y,'severe'), !.
+	write_ln('Nível alto de transtorno').
--- src/main.pl
+++ src/main.pl
@@ -1,2 +1,39 @@
 :-include('base.pl').
+:-include('perguntas.pl').
 
+
+% Predicado que calcula o nivel de transtorno
+questionario :-
+	pergunta1(0,Depre),
+	pergunta2(0,Ans),
+	pergunta3(0,Str),
+	pergunta4(Ans,Ans2),
+	pergunta5(Depre,Depre2),
+	pergunta6(Str,Str2),
+	pergunta7(Ans2,Ans3),
+	pergunta8(Str2,Str3),
+	pergunta9(Ans3,Ans4),
+	pergunta10(Depre2,Depre3),
+	pergunta11(Str3,Str4),
+	pergunta12(Str4,Str5),
+	pergunta13(Depre3,Depre4),
+	pergunta14(Str5,Str6),
+	pergunta15(Ans4,Ans5),
+	pergunta16(Depre4,Depre5),
+	pergunta17(Depre5,Depre6),
+	pergunta18(Str6,Str7),
+	pergunta19(Ans5,Ans6),
+	pergunta20(Ans6,Ans7),
+	pergunta21(Depre6,Depre7),
+	X = Depre7,
+	Y = Ans7,
+	Z = Str7,
+	writeln('Transtornos'),
+	writeln(''),
+	write('Depressão: '),
+	grau(doenca(deprecao),X),
+	write('Ansiedade: '),
+	grau(doenca(ansiedade),Y),
+	write('Estresse: '),
+	grau(doenca(stress),Z),
+	writeln('').
