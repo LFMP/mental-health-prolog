e19231bb6febfb5887107d4640ccd178a12c03ce
LFMP <luizfellipe42@hotmail.com>
2020-01-15 19:32:56 -0300

    fix erros de portugues e add ajuda ao usuario

--- src/base.pl
+++ src/base.pl
@@ -1,28 +1,28 @@
 :-include('perguntas.pl').
-:- use_module(library(plunit)).
+:-use_module(library(plunit)).
 % Doencas a serem detectadas
-doenca(deprecao).
+doenca(depressao).
 doenca(ansiedade).
 doenca(stress).
 
 % O grau de cada doenca teve como base o questionario DASS 21
 % a soma maxima para cada doenca eh de 21 pontos
 
-%% grau(+doenca, +X, ?Y)(+X, ?Y) is det
+%% grau(+doenca, +X, ?Y) is det
 %
 % Verdadeiro se X pertence ao valor determinado para cada nivel de doenca.
-grau(doenca(deprecao), X, Y) :-
+grau(doenca(depressao), X, Y) :-
     X<7,
     Y is 0,
     !.
 
-grau(doenca(deprecao), X, Y) :-
+grau(doenca(depressao), X, Y) :-
     X>=7,
     X<14,
     Y is 1,
     !.
 
-grau(doenca(deprecao), X, Y) :-
+grau(doenca(depressao), X, Y) :-
     X>=14,
     Y is 2,
     !.
@@ -62,13 +62,13 @@ grau(doenca(stress), X, Y) :-
 
 :-begin_tests(graudoenca).
 
-test(semDeprecao) :- grau(doenca(deprecao), 6, 0).
+test(semDepressao) :- grau(doenca(depressao), 6, 0).
 test(semAnsiedade) :- grau(doenca(ansiedade), 5, 0).
 test(semStress) :- grau(doenca(stress), 9, 0).
-test(mediaDeprecao) :- grau(doenca(deprecao), 10, 1).
+test(mediaDepressao) :- grau(doenca(depressao), 10, 1).
 test(mediaAnsiedade) :- grau(doenca(ansiedade), 9, 1).
 test(medioStress) :- grau(doenca(stress), 12, 1).
-test(altaDeprecao) :- grau(doenca(deprecao), 17, 2).
+test(altaDepressao) :- grau(doenca(depressao), 17, 2).
 test(altaAnsiedade) :- grau(doenca(ansiedade), 12, 2).
 test(altoStress) :- grau(doenca(stress), 20, 2).
 
@@ -80,15 +80,15 @@ diagnostico(X) :-
 
 diagnostico(X) :-
 	X == 1,
-	write_ln('Nível médio de transtorno'), !.
+	write_ln('Nível médio do transtorno'), !.
 
 diagnostico(X) :-
 	X == 2,
-	write_ln('Nível alto de transtorno'), !.
+	write_ln('Nível alto do transtorno'), !.
 
 %% questionario(+X, +Y, +Z) is det
 % Predicado que calcula o nivel de transtorno
-questionario(Deprecao, Ansiedade, Stress) :-
+questionario(Depressao, Ansiedade, Stress) :-
 	pergunta1(0,Depre),
 	pergunta2(0,Ans),
 	pergunta3(0,Str),
@@ -113,8 +113,8 @@ questionario(Deprecao, Ansiedade, Stress) :-
 	X = Depre7,
 	Y = Ans7,
 	Z = Str7,
-	grau(doenca(deprecao),X, SaidaD),
-	Deprecao is SaidaD,
+	grau(doenca(depressao),X, SaidaD),
+	Depressao is SaidaD,
 	grau(doenca(ansiedade),Y,SaidaA),
 	Ansiedade is SaidaA,
 	grau(doenca(stress),Z, SaidaS),
--- src/main.pl
+++ src/main.pl
@@ -1,8 +1,25 @@
 :- include('base.pl').
 
+:- writeln("================================================================================================================").
+:- writeln("A saúde mental inclui nosso bem-estar emocional, psicológico e social. ").
+:- write("Afeta como pensamos, sentimos e agimos.").
+:- writeln(" Também ajuda a determinar como lidamos com o estresse,").
+:- writeln("nos relacionamos com os outros e fazemos escolhas. ").
+:- writeln("A saúde mental é importante em todas as fases da vida, desde a infância e a adolescência até a idade adulta.").
+:- writeln("Para proseguir com o teste de estresse, depressão e ansiedade digite o seguinte comando (incluido o ponto):").
+:- writeln("start.").
+:- writeln("================================================================================================================").
 %% grau() is det
 % predicado que carrega todo o sistema e apresenta o resultado do teste
-start:-
+start :-
+    nl,
+    write("Este teste da escala de estresse, ansiedade e depressão (DASS 21) avalia a gravidade "),
+    writeln("dos sintomas de transtorno mental associados aos três e fornece um resultado leve, moderado ou grave."),
+    write("Este teste não é um diagnóstico por si só, é apenas um esquema de autorrelato "),
+    writeln("e deve ser seguido por uma avaliação psicológica ou psiquiátrica completa."),
+    write("Para cada pergunta responda com um número inteiro de 0 a 3 seguinte de um ponto de acordo com a sua frequencia,"),
+    writeln("onde 0 corresponde à 'nunca', 1 à 'às vezes', 2 à 'frequentemente' e 3 à 'quase sempre'."),
+    nl,
     questionario(A, B, C),
     writeln(''),
     writeln('Transtornos'),
